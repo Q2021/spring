e88b639739aecd54380d177fc7462ccc38ce4005
Q2021 <654830598@qq.com>
2021-01-01 18:05:31 +0800

    手写AutoWired注解

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -1,9 +1,11 @@
 package org.example.controller;
 
+import org.example.annotation.AutoWired;
 import org.example.service.UserService;
 
 public class UserController {
 
+    @AutoWired
     private UserService userService;
 
     public UserService getUserService() {
--- src/test/java/org/example/MyTest.java
+++ src/test/java/org/example/MyTest.java
@@ -1,16 +1,18 @@
 package org.example;
 
+import org.example.annotation.AutoWired;
 import org.example.controller.UserController;
 import org.example.service.UserService;
 import org.junit.Test;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.stream.Stream;
 
 public class MyTest {
 
     @Test
-  public void test() throws Exception {
+  public void test01() throws Exception {
         UserController userController = new UserController();
         Class<? extends UserController> clazz = userController.getClass();
         //创建对象
@@ -26,6 +28,30 @@ public class MyTest {
         //通过方法属性注入属性的对象
         Method method = clazz.getMethod(setMethodName, UserService.class);
         method.invoke(userController, userService);
+        System.out.println(userController.getUserService());
+    }
+
+    @Test
+    public void test02() {
+        UserController userController = new UserController();
+        Class<? extends UserController> clazz = userController.getClass();
+        //获取所有属性值
+        Stream.of(clazz.getDeclaredFields()).forEach(field -> {
+            AutoWired annotation = field.getAnnotation(AutoWired.class);
+            if (annotation != null) {
+                field.setAccessible(true);
+                //获取属性类型
+                Class<?> type = field.getType();
+                try {
+                    Object o = type.newInstance();
+                    field.set(userController, o);
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         System.out.println(userController.getUserService());
     }
 }
